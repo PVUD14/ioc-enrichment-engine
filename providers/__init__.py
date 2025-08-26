ed453304eeadf4e630dadf9cbd94fe7dc74a488c
P_V_U_Divakar <131444762+PVUD14@users.noreply.github.com>
2025-08-26 12:22:38 +0530

    Update __init__.py

--- providers/__init__.py
+++ providers/__init__.py
@@ -1,1 +1,15 @@
+from typing import Callable, Dict
 
+_REGISTRY: Dict[str, Callable] = {}
+
+def provider_name(name: str):
+    def deco(fn):
+        _REGISTRY[name] = fn
+        return fn
+    return deco
+
+def get_provider(name: str) -> Callable:
+    return _REGISTRY[name]
+
+def all_providers():
+    return list(_REGISTRY.keys())
